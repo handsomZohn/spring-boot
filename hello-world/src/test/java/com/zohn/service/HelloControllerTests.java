0a39f041b770d344127e90596bf5a4d2eed01f4d
张毅峰 <zhangyifeng@jsb5.com>
2018-02-23 14:29:11 +0800

    单元测试  修改为自动装配

--- hello-world/src/test/java/com/zohn/service/HelloControllerTests.java
+++ hello-world/src/test/java/com/zohn/service/HelloControllerTests.java
@@ -3,9 +3,12 @@ package com.zohn.service;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -13,16 +16,24 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 /**
- * Created by zohn on 2017/12/8.
+ * Created by zohn on 2017/12/8
+ *编写简单的http请求来测试；使用mockmvc进行，利用MockMvcResultHandlers.print()打印出执行结果
+ *
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HelloControllerTests {
+
     private MockMvc mvc;
+
+    @Autowired
+    HelloWorldController helloWorldController;
 
     @Before
     public void setUp() throws  Exception{
-        mvc = MockMvcBuilders.standaloneSetup(new HelloWorldController()).build();
+        //需要的时候去new一个对象  改为自动装配
+        //mvc = MockMvcBuilders.standaloneSetup(new HelloWorldController()).build();
+        mvc = MockMvcBuilders.standaloneSetup(helloWorldController).build();
     }
 
     @Test
